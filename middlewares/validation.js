7b6cad29031cb343149a2bcc1157c3d5d0b61348
Steven Bolin <SABolin1@me.com>
2026-03-02 12:24:30 -0500

    joi validation started

---
+++ middlewares/validation.js
@@ -0,0 +1,53 @@
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
+imageUrl: Joi.string().required().custom(validateURL).messages({
+  "string.empty": 'The "imageUrl" field must be filled in',
+  "string.uri": 'the "imageUrl" field must be a valid url',
+}),
+  (module.exports.validateCardBody = celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30).messages({
+        "string.min": 'The minimum length of the "name" field is 2',
+        "string.max": 'The maximum length of the "name" field is 30',
+        "string.empty": 'The "name" field must be filled in',
+      }),
+
+      imageUrl: Joi.string().required().custom(validateURL).messages({
+        "string.empty": 'The "imageUrl" field must be filled in',
+        "string.uri": 'the "imageUrl" field must be a valid url',
+      }),
+    }),
+  }));
+
+module.exports.validateUserBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+
+    avatarUrl: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatarUrl" field must be filled in',
+      "string.uri": 'the "avatarUrl" field must be a valid url',
+    }),
+
+    email: Joi.string().required().email().messages({
+      "string.email": 'The "email" field must be a valid email address',
+      "string.empty": 'The "email" field must be filled in',
+    }),
+
+    password: Joi.string().required().min(6).messages({
+      "string.min": 'The minimum length of the "password" field is 6',
+      "string.empty": 'The "password" field must be filled in',
+    }),
+  }),
+});
