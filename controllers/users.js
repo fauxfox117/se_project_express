2c59a21b99d439a1a1cc8b4fab086cfa540e497d
Steven Bolin <SABolin1@me.com>
2026-03-14 16:05:03 -0400

    removed getUsers, getUser

--- controllers/users.js
+++ controllers/users.js
@@ -14,30 +14,6 @@ const ConflictError = require("../errors/conflict-err");
 const NotFoundError = require("../errors/not-found-err");
 const BadRequestError = require("../errors/bad-request-err");
 
-// const getUsers = (req, res) => {
-//   User.find({})
-//     .then((users) => res.send(users))
-//     .catch(() =>
-//       res
-//         .status(INTERNAL_SERVER_ERROR)
-//         .send({ message: "An error occurred on the server" })
-//     );
-// };
-
-// const getUser = (req, res, next) => {
-//   User.findById(req.params.userId)
-//     .orFail()
-//     .then((user) => res.send(user))
-//     .catch((err) => {
-//       if (err.name === "DocumentNotFoundError") {
-//         return next(new NotFoundError("User not found"));
-//       } else if (err.name === "CastError") {
-//         return next(new BadRequestError("Invalid user ID"));
-//       }
-//       return next(err);
-//     });
-// };
-
 const createUser = (req, res, next) => {
   const { name, avatar, email, password } = req.body;
 
